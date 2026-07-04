f73c4f6e3087064c9d9d0138e14ba2735f913cb2
nico-leitz <ndesk.ltrz@gmail.com>
2026-07-04 15:37:53 +0200

    feat(contact-list): implement HTML structure with test data and interface for contact objects

--- src/app/features/contacts/components/contact-list/contact-list.ts
+++ src/app/features/contacts/components/contact-list/contact-list.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
 
+interface contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email?: string; 
+}
+
 @Component({
   selector: 'app-contact-list',
   imports: [],
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.scss',
 })
-export class ContactList {}
+export class ContactList {
+  contacts: contact[] = [
+    {
+    id: 1,
+    firstName: "Test",
+    lastName: "Testermann",
+    email: "Test@testmail.de"
+    },
+    {
+    id: 2,
+    firstName: "Test2",
+    lastName: "Testermann2",
+    email: "Test@testmail2.de"
+    }
+
+  ] 
+}
