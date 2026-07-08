5230c3f168dda105e1848e5b634a2c8153530327
CZX93-BW <yunimoon2012@gmail.com>
2026-07-08 12:07:00 +0200

    feat(layout): implement app shell with sidebar and header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './layout/sidebar/sidebar';
 import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Sidebar, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
