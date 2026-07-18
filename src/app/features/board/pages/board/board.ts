6297359fc10a34ed580d376ff0bac0075d1974d3
nico-leitz <ndesk.ltrz@gmail.com>
2026-07-18 20:11:51 +0200

    feat(board): implement Drag and Drop mechanics via Angular Docs

--- src/app/features/board/pages/board/board.ts
+++ src/app/features/board/pages/board/board.ts
@@ -1,15 +1,30 @@
 import { Component, signal } from '@angular/core';
-import { TaskCard } from "../../components/task-card/task-card";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  CdkDragHandle,
+} from '@angular/cdk/drag-drop';
+import { TaskCard } from '../../components/task-card/task-card';
 import { BoardCardsDialog } from '../../../tasks/components/board-cards-dialog/board-cards-dialog';
 
 @Component({
   selector: 'app-board',
-  imports: [TaskCard, BoardCardsDialog],
+  standalone: true,
+  imports: [TaskCard, BoardCardsDialog, CdkDropListGroup, CdkDropList, CdkDrag, CdkDragHandle],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
 export class Board {
   isDialogOpen = signal(false);
+
+  todo = ['Task 1', 'Task 2'];
+  inProgress = ['Task 3'];
+  awaitFeedback = ['Task 4'];
+  done = ['Task 5'];
 
   openDialog(): void {
     this.isDialogOpen.set(true);
@@ -17,5 +32,18 @@ export class Board {
 
   closeDialog(): void {
     this.isDialogOpen.set(false);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
