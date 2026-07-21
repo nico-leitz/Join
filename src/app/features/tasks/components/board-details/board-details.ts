b70c54feebd31c9d052d0e29885dfedc850272f3
CZX93-BW <yunimoon2012@gmail.com>
2026-07-21 22:38:26 +0200

    refactor(board): remove unused board column & details component

--- src/app/features/tasks/components/board-details/board-details.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-board-details',
-  imports: [],
-  templateUrl: './board-details.html',
-  styleUrl: './board-details.scss',
-})
-export class BoardDetails {}
