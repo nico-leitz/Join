c547b32b089b3a40d84cc0bda16ed0476e66f131
CZX93-BW <yunimoon2012@gmail.com>
2026-07-23 01:32:35 +0200

    refactor(add-task): remove obsolete task components

--- src/app/features/tasks/components/add-task-details/add-task-details.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-add-task-details',
-  imports: [],
-  templateUrl: './add-task-details.html',
-  styleUrl: './add-task-details.scss',
-})
-export class AddTaskDetails {}
--- src/app/features/tasks/pages/add-task/add-task.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-add-task',
-  imports: [],
-  templateUrl: './add-task.html',
-  styleUrl: './add-task.scss',
-})
-export class AddTask {}
