af3d99e0f6bf969d4a00c09d05c7014742b584eb
nico-leitz <ndesk.ltrz@gmail.com>
2026-07-24 16:02:09 +0200

    feat(auth-page): import and implement Login and Sign-Up

--- src/app/features/auth/auth-page/auth-page.ts
+++ src/app/features/auth/auth-page/auth-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Login } from '../pages/login/login';
+import { Signup } from '../pages/signup/signup';
 
 @Component({
   selector: 'app-auth-page',
-  imports: [Login],
+  imports: [Login, Signup],
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.scss',
 })
